a13e3f9ecc7e57b4ca3297ff0279aa48f917d42a
Jacob Kania <25391879+jacobkania@users.noreply.github.com>
2020-12-14 01:03:34 -0800

    Added snowpack and svelte dependencies

---
+++ js/snowpack.config.js
@@ -0,0 +1,27 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+	  '@snowpack/plugin-svelte'
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
